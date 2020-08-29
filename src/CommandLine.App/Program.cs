e3183e1a0d08c0ef16d8530873355dce30fec361
greenSacrifice <emails@joshuavandaalen.com.au>
2020-08-29 22:17:22 +1000

    add simple comand line argument options

--- src/CommandLine.App/Program.cs
+++ src/CommandLine.App/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using CommandLine.Text;
+using System;
+using System.Collections.Generic;
 
 namespace CommandLine.App
 {
+    internal class CommandLineArgs
+    {
+        [Option('f', "firstname", Required = true, HelpText = "First name")]
+        public string FirstName { get; set; }
+
+        [Option('l', "lastname", Required = true, HelpText = "Last Name")]
+        public string LastName { get; set; }
+
+        [Usage(ApplicationAlias = nameof(CommandLineArgs))]
+        public static IEnumerable<Example> Examples => new List<Example>
+        {
+            new Example("Using your name as arguments, this app will display your name.", new CommandLineArgs
+            {
+                FirstName = "Joshua",
+                LastName = "Van Daalen",
+            })
+        };
+    }
     class Program
     {
-        static void Main(string[] args)
+        static CommandLineArgs _options;
+
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = Parser.Default.ParseArguments<CommandLineArgs>(args).MapResult(
+                   (CommandLineArgs opts) => Run(opts),
+                   errs => 1);
+
+            return result;
+        }
+        private static int Run(CommandLineArgs options)
+        {
+            _options = options;
+
+            Console.WriteLine($"{_options.FirstName} {_options.LastName}");
+
+            return 0;
         }
     }
 }
